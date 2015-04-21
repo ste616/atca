61b3a0297d2fdba7dc273e44758d6621540885f7
Jamie Stevens <ste616@gmail.com>
2015-04-21 15:28:44 +1000

    Added help text about the weather conditions to the calculator help page, and fixed some bugs regarding the display of the help page.

--- atsenscalc/web/interactive_senscalc_help.js
+++ atsenscalc/web/interactive_senscalc_help.js
@@ -1,10 +1,22 @@
-require( [ 'dojo/query', 'dojo/hash', 'dojo/io-query', 'dojo/NodeList-dom' ],
-	 function(query, hash, ioQuery) {
+require( [ 'dojo/query', 'dojo/hash', 'dojo/io-query', 'dojo/topic', 'dojo/NodeList-dom' ],
+	 function(query, hash, ioQuery, topic) {
+
 	     // Get the id to show from the address bar.
-	     var o = ioQuery.queryToObject(hash());
-	     for (var p in o) {
-		 if (o.hasOwnProperty(p)) {
-		     query("#" + p).removeClass('invisible');
+	     var showHelp = function() {
+		 var o = ioQuery.queryToObject(hash());
+		 for (var p in o) {
+		     if (o.hasOwnProperty(p)) {
+			 query("#" + p).removeClass('invisible');
+		     }
 		 }
-	     }
+	     };
+	     // Do this on page load.
+	     showHelp();
+
+	     // Look out for changes in the hash.
+	     topic.subscribe('/dojo/hashchange', function() {
+		 // Hide all the help sections first.
+		 query('.helpSection').addClass('invisible');
+		 showHelp();
+	     });
 	 });
